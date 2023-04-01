07952cde0ee208aac8181fda99df1964c2f51554
Kruk2 <Kruk2>
2023-04-01 18:20:38 +0200

    increased size to 10k

--- ui/v2.5/src/components/SceneFilenameParser/ParserInput.tsx
+++ ui/v2.5/src/components/SceneFilenameParser/ParserInput.tsx
@@ -128,7 +128,7 @@ export const ParserInput: React.FC<IParserInputProps> = (
     setPattern(pattern + field.getFieldPattern());
   }
 
-  const PAGE_SIZE_OPTIONS = ["20", "40", "60", "120", "250", "500", "1000"];
+  const PAGE_SIZE_OPTIONS = ["20", "40", "60", "120", "250", "500", "1000", "10000"];
 
   return (
     <Form.Group>
